748fcb28c63dcb8c0be576063cc0f83a5e09dd39
Garo-A <garo.assilian@gmail.com>
2017-07-03 19:07:26 +0000

    Added header to make request

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
@@ -6,9 +7,29 @@ console.log("Welcome to the GitHub Avatar Downloader!");
 var GITHUB_USER = "Garo-A";
 var GITHUB_TOKEN = "a78268fdb4405dfd0d89420a01fb5fbdd79eaae0";
 
+
+
 function getRepoContributors(repoOwner, repoName, cb){
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors'; //Gets you to the contributors endpoint
-  console.log(requestURL);
+
+
+  var options = {
+    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'request'
+    }
+  }
+
+  request.get(options) //REQUEST STARTS HERE
+
+    .on('error', function(err){ //handles Errors
+      throw err;
+    })
+
+    .on('response', function(response){ //Shows me if my response went through, and if it didn't, which code and message.
+      console.log("Response Code: ", response.statusCode);
+      console.log("Response Message: ", response.statusMessage);
+    })
+
 }
 
 getRepoContributors('jquery','jquery');
