44bead08018b9b2414b0a514459260588388c148
Garo-A <garo.assilian@gmail.com>
2017-07-03 21:12:28 +0000

    Added command line arguments and error message

--- download_avatars.js
+++ download_avatars.js
@@ -3,45 +3,22 @@ var fs = require('fs');
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
+var args = process.argv.slice(2);
+
+var owner = args[0];
+var repo = args[1];
+
 
 var GITHUB_USER = "Garo-A";
 var GITHUB_TOKEN = "a78268fdb4405dfd0d89420a01fb5fbdd79eaae0";
 
 
-function printUser(array){ //CALLBACK -- Takes JSON array, goes through array creating image URL and then passes that thorugh downloader/
+function printUser(array){ //CALLBACK -- Takes JSON array, goes through array creating image URL and then passes that thorugh downloader. Also setsd filepath as login
   for(var i = 0; i < array.length; i++) {
     var path = array[i].login;
     downloadImageByURL(array[i].avatar_url + "avatars/" + array[i].login + ".jpg", path);
   }
 }
-
-
-function getRepoContributors(repoOwner, repoName, cb){
-
-  var data = "";
-  var options = {
-    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-    headers: {
-      'User-Agent': 'request'
-    }
-  }
-
-  request.get(options, function(error,response,body){ //REQUEST STARTS HERE
-    if(error){
-      throw error;
-    }
-
-    console.log("Response Code: ", response.statusCode);
-    console.log("Response Message: ", response.statusMessage);
-
-
-    cb(JSON.parse(body)); //THIS BASICALLY GENERATES THE URL
-  })
-}
-
-getRepoContributors('jquery','jquery', printUser);
-
-
 
 function downloadImageByURL (url, filepath) { //DOWNLOADER -- Takes a given URL and fwets into it, gets image, downloads it using login as name.
 
@@ -49,3 +26,45 @@ function downloadImageByURL (url, filepath) { //DOWNLOADER -- Takes a given URL 
 
     .pipe(fs.createWriteStream('./'+ filepath + '.jpg'));
 }
+
+
+function getRepoContributors(repoOwner, repoName, cb){
+
+  if (repoOwner === undefined || ""){
+  console.log("Please enter a value for owner field")
+  return;
+  }
+
+  else if (repoName === undefined || ""){
+    console.log("Please enter a value for the repo field")
+    return;
+  }
+
+  else {
+    var options = {
+      url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+      headers: {
+        'User-Agent': 'request'
+      }
+    }
+
+    request.get(options, function(error,response,body){ //REQUEST STARTS HERE
+      if(error){ //handles error.
+        throw error;
+      }
+      // Following code is to see if there is an error getting the response and what code and message it returns
+      console.log("Response Code: ", response.statusCode);
+      console.log("Response Message: ", response.statusMessage);
+
+      // callback function actually executing the entire thing. Will take JSON object and feed it into callback as an array.
+      cb(JSON.parse(body));
+    })
+  }
+}
+
+getRepoContributors(owner,repo,printUser);
+
+
+
+
+
